f95d5c25e4644c85fab8126ee21b35ee12e7937e
Hu Ziming <hzmangel@gmail.com>
2015-12-08 21:48:33 +0800

    Add belongs_to field for materials table

--- db/migrate/20151208132905_add_material_to_cookbook.rb
+++ db/migrate/20151208132905_add_material_to_cookbook.rb
@@ -1,5 +1,6 @@
 class AddMaterialToCookbook < ActiveRecord::Migration
   def change
     add_reference :cookbooks, :material, index: true, foreign_key: true
+    add_reference :materials, :cookbook, index: true, foreign_key: true
   end
 end
