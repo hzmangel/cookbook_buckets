7ace842daec2fe59f12181be826025bf671695b6
Hu Ziming <hzmangel@gmail.com>
2015-12-08 21:49:06 +0800

    Rename table and migration file name to match rules of migration.

--- db/migrate/20151208133154_cookbooks_tags.rb
+++ db/migrate/20151208133154_cookbooks_tags.rb
@@ -1,6 +1,6 @@
-class TagsCookbooks < ActiveRecord::Migration
+class CookbooksTags < ActiveRecord::Migration
   def change
-    create_table :tags_cookbooks do |t|
+    create_table :cookbooks_tags, id: false do |t|
       t.belongs_to :cookbook, index: true
       t.belongs_to :tag, index: true
     end
