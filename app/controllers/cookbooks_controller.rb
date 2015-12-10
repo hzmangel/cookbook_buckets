27de3e8484917f039fb562457b766d18dedbb087
Hu Ziming <hzmangel@gmail.com>
2015-12-10 09:11:24 +0800

    Allow post w/o form token

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -2,6 +2,7 @@
 # AngularJS so no new and edit in actions.
 class CookbooksController < ApplicationController
   before_action :prepare_rcd, only: [:show, :update, :destroy]
+  skip_before_filter :verify_authenticity_token
 
   def index
     @rcds = Cookbook.all
