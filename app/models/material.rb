757b3b889c4858651bed92868448f89d5533af05
Hu Ziming <hzmangel@gmail.com>
2015-12-10 11:05:49 +0800

    Fix variable name error

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,6 +3,6 @@ class Material < ActiveRecord::Base
   belongs_to :cookbook
 
   validates :name, presence: true
-  validates :quality, numericality: { only_integer: true, greater_than: 0 }
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   validates :unit, presence: true
 end
