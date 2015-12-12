243082dcac336d41408ef00bb6bf42d2b9586a06
Hu Ziming <hzmangel@gmail.com>
2015-12-12 11:43:27 +0800

    Update case to match new format of params

--- spec/controllers/cookbooks_controller_spec.rb
+++ spec/controllers/cookbooks_controller_spec.rb
@@ -3,27 +3,31 @@ require 'rails_helper'
 RSpec.describe CookbooksController, type: :controller do
   describe '#create' do
     context 'when given valid params' do
-      let(:valid_params) do
+      let(:cookbook_params) do
+        { name: 'Cookbook 1', desc: 'Desc of Cookbook 1' }
+      end
+
+      let(:materials_params) do
+        [
+          { name: 'material_1', quantity: 100, unit: 'g' },
+          { name: 'material_2', quantity: 10, unit: 'cm' }
+        ]
+      end
+
+      let(:tags_params) do
+        [{ text: 'tag_1' }, { text: 'tag_2' }]
+      end
+
+      let(:params) do
         {
-          cookbook: {
-            name: 'Cookbook_1',
-            desc: 'Desc of Cookbook_1',
-            materials_attributes: [{
-              name: 'material_1',
-              quantity: 100,
-              unit: 'g'
-            }, {
-              name: 'material_2',
-              quantity: 10,
-              unit: 'cm'
-            }],
-            tags_attributes: [{ name: 'tag_1' }, { name: 'tag_2' }]
-          }
+          cookbook: cookbook_params,
+          materials: materials_params,
+          tags: tags_params
         }
       end
 
       subject(:response) do
-        post :create, valid_params
+        post :create, params
       end
 
       it { expect { subject }.to change(Cookbook, :count).by(1) }
@@ -40,18 +44,18 @@ RSpec.describe CookbooksController, type: :controller do
     end
 
     context 'when adding materials' do
-      let(:params) do
-        {
-          materials_attributes: [{
-            name: 'NewAddedMaterial',
-            quantity: 20,
-            unit: 'kg'
-          }]
-        }
+      let(:materials_params) do
+        JSON.parse(cookbook.materials.to_json)
+          .append(name: 'NewAddedMaterial',
+                  quantity: 20,
+                  unit: 'kg')
       end
 
       subject(:response) do
-        patch :update, id: cookbook.id, cookbook: params
+        patch :update, id: cookbook.id,
+                       cookbook: JSON.parse(cookbook.to_json),
+                       tags: JSON.parse(cookbook.tags.to_json),
+                       materials: materials_params
       end
 
       it { expect { subject }.not_to change(Cookbook, :count) }
@@ -59,18 +63,38 @@ RSpec.describe CookbooksController, type: :controller do
       it { expect(subject.status).to eq 200 }
     end
 
-    context 'when removing materials' do
-      let(:params) do
-        {
-          materials_attributes: [{
-            id: cookbook.materials.first.id,
-            _destroy: true
-          }]
-        }
+    context 'when update materials' do
+      let(:materials_params) do
+        materials_json = JSON.parse(cookbook.materials.to_json)
+        materials_json.first[:name] = 'ChangeMaterialName'
+        materials_json
       end
 
       subject(:response) do
-        patch :update, id: cookbook.id, cookbook: params
+        patch :update, id: cookbook.id,
+                       cookbook: JSON.parse(cookbook.to_json),
+                       tags: JSON.parse(cookbook.tags.to_json),
+                       materials: materials_params
+      end
+
+      it { expect { subject }.not_to change(Cookbook, :count) }
+      it { expect { subject }.not_to change(Material, :count) }
+      it { expect(subject.status).to eq 200 }
+      it { expect { subject }.to change(Material.where(name: 'ChangeMaterialName'), :count).by(1) }
+    end
+
+    context 'when removing materials' do
+      let(:materials_params) do
+        materials_json = JSON.parse(cookbook.materials.to_json)
+        materials_json.first[:_destroy] = true
+        materials_json
+      end
+
+      subject(:response) do
+        patch :update, id: cookbook.id,
+                       cookbook: JSON.parse(cookbook.to_json),
+                       tags: JSON.parse(cookbook.tags.to_json),
+                       materials: materials_params
       end
 
       it { expect { subject }.not_to change(Cookbook, :count) }
